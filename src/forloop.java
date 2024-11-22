30e53ef55cbacac507daea8f104f6244e89da4f0
Poojashinde-9529 <poojashinde0283@gmail.com>
2024-11-22 22:15:37 +0530

    first commit

---
+++ src/forloop.java
@@ -0,0 +1,13 @@
+public class forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=5;i<=25;i++)
+		{
+			System.out.println(i);
+		}
+		
+	}
+
+}
